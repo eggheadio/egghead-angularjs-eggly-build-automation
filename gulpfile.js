ea46f69807bea9a8200a7976eb61f1fc7f4244bb
Lukas Ruebbelke <simpul@gmail.com>
2015-03-10 20:20:07 -0700

    Added copy tasks
    Installed del and run-sequence

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  del = require('del'),
+  runSequence = require('run-sequence');
 
-gulp.task('default', function () {
-    console.log("HELLO GULP!")
+gulp.task('default', function(callback) {
+  runSequence('build', callback);
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean',
+    'copy-build',
+    callback);
+});
+
+gulp.task('clean', function (callback) {
+  del(['./build'], {force: true}, callback)
+});
+
+gulp.task('copy-build', ['copy-assets', 'copy-app-js', 'copy-vendor-js']);
+
+gulp.task('copy-assets', function () {
+  return gulp.src('./src/assets/**/*')
+    .pipe(gulp.dest('./build/assets'));
+});
+
+gulp.task('copy-app-js', function () {
+  return gulp.src(files.app_files.js)
+    .pipe(gulp.dest('./build'));
 });
