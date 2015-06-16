5268ff20c41810b8d72855a349c0d2b1654ec444
Lukas Ruebbelke <simpul@gmail.com>
2015-06-16 10:15:43 -0700

    Changes for gulp tasks lesson

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  runSequence = require('run-sequence');
 
-gulp.task('default', function () {
-    console.log("HELLO GULP!")
+gulp.task('default', function (callback) {
+    runSequence('build', callback);
+});
+
+gulp.task('build',function(callback){
+    runSequence('clean', 'copy-build', callback);
+});
+
+gulp.task('clean',function(){
+
+});
+
+gulp.task('copy-build', ['copy-assets', 'copy-app-js', 'copy-vendor-js']);
+
+gulp.task('copy-assets',function(){
+
+});
+
+gulp.task('copy-app-js',function(){
+
+});
+
+gulp.task('copy-vendor-js',function(){
+
 });
